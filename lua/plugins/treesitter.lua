757a4998c0578db2d32039865849917df7c7aefb
Cyrus <86699936+cyrusae@users.noreply.github.com>
2026-01-04 14:24:26 -0800

    force inbuilt compiler

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,11 +4,15 @@ return {
   opts = {
     ensure_installed = { 
       "lua", "vim", "vimdoc", 
-      "markdown", "markdown_inline",  -- Add these for render-markdown
+      "markdown", "markdown_inline",
       "python", "javascript", "bash"
     },
     auto_install = true,
     highlight = { enable = true },
     indent = { enable = true },
   },
+  -- Force use of built-in compiler
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
