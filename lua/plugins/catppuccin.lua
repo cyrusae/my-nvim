d2b2fcc62ebdcc8172ddcb3f879643f46f2eb924
Cyrus <86699936+cyrusae@users.noreply.github.com>
2026-01-04 22:10:22 -0800

    Enhance Catppuccin plugin with markdown support

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,15 +3,29 @@ return {
   name = "catppuccin",
   priority = 1000,
   opts = {
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    flavour = "mocha",
     transparent_background = false,
     integrations = {
       treesitter = true,
-      native_lsp = {
-        enabled = true,
-      },
+      native_lsp = { enabled = true },
       telescope = true,
       which_key = true,
+      markdown = true,  -- Add markdown support
+      render_markdown = true,  -- And markdown support
     },
+    custom_highlights = function(colors)
+      return {
+        -- Customize markdown highlights
+        ["@markup.heading.1.markdown"] = { fg = colors.red, bold = true },
+        ["@markup.heading.2.markdown"] = { fg = colors.peach, bold = true },
+        ["@markup.heading.3.markdown"] = { fg = colors.yellow, bold = true },
+        ["@markup.heading.4.markdown"] = { fg = colors.green, bold = true },
+        ["@markup.heading.5.markdown"] = { fg = colors.sapphire, bold = true },
+        ["@markup.heading.6.markdown"] = { fg = colors.lavender, bold = true },
+        ["@markup.strong.markdown_inline"] = { fg = colors.mauve, bold = true },
+        ["@markup.italic.markdown_inline"] = { fg = colors.blue, italic = true },
+        ["@markup.link.markdown_inline"] = { fg = colors.sky, underline = true },
+      }
+    end,
   },
 }
